3337e27575ca888fcfe7b48d8aaaa828bcac7e9b
Rakshana Devalla <140353493+RakshanaD@users.noreply.github.com>
2024-11-27 10:42:37 -0500

    Part 1 final commit
    Adds all numbers b/w parameters

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("hi");
+        Range sum1 = (start, end) -> {
+            int count = 0;
+            for(int i = start; i<=end; i++)
+                count += i;
+            return count;
+        };
+
+
+        System.out.println(sum1.sum(Integer.parseInt(args[0]),Integer.parseInt(args[1])));
+
     }
 }
