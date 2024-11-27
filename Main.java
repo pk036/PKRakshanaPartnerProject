6b3542132ada4834eb5feecf9814c252d810ef33
Rakshana Devalla <140353493+RakshanaD@users.noreply.github.com>
2024-11-27 10:14:21 -0500

    Interface commit
    Possibly

--- Main.java
+++ Main.java
@@ -1,15 +1,5 @@
-public class Main implements Range{
+public class Main {
     public static void main(String[] args) {
 
-    }
-
-    @Override
-    public int sum(int start, int end) {
-        int sum = 0;
-        for(int i = start; i<end; i++)
-        {
-            sum+=i;
-        }
-        return sum;
     }
 }
